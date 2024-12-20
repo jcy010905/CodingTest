bf793c69ec7c1abac9543005dc5c979b0ae064f8
Jung chan yong <155941775+jcy010905@users.noreply.github.com>
2024-12-20 13:19:45 +0900

    Add files via upload

---
+++ 문자열 집합.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+s = set()
+for _ in range(n):
+    s.add(input().strip())
+
+count = 0
+for _ in range(m):
+    if input().strip() in s:
+        count += 1
+
+print(count)
